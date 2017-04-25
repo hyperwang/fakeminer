5af42017cc837ae67fefc5b3075e6a9001c82e29
HyperWang <hyperwangee@gmail.com>
2017-04-25 20:02:15 +0800

    add miner & shorten sleep

--- fakeminer.py
+++ fakeminer.py
@@ -57,7 +57,7 @@ class StratumSession():
         self.exn1 = exn1
         self.exn2_size = exn2_size
         logging.debug("Set extra nounce 1 value=%s, extra nounce 2 size=%s", self.exn1, self.exn2_size)
-    
+
     def set_job(self, job_params):
         self.job = StratumJob(self.client.pool, self.client.user, job_params)
 
@@ -66,25 +66,25 @@ class StratumSession():
         for ts, diff in self.share_list:
             if now_ts - ts > 60:
                 self.share_list.remove((ts, diff))
-        pow_in_minute = sum([diff for _, diff in self.share_list]) 
+        pow_in_minute = sum([diff for _, diff in self.share_list])
         target_pow = self.pow_per_sec * 60
         logging.debug("diff: %d, pow in minute: %d, target pow in minute: %d", self.diff, pow_in_minute, target_pow)
-        return pow_in_minute < target_pow 
-    
+        return pow_in_minute < target_pow
+
     def submit_share(self):
         self.share_list.append((int(time.time()), self.diff))
 
     def __str__(self):
         return "pool(%s),user(%s),pow(%d),job(%s)"%(self.client.pool, self.client.user, self.pow_per_sec, self.job)
-    
+
 
 class StratumClientProtocol(asyncio.Protocol):
     STATE_IDLE = 0b00000000
     STATE_CONN = 0b00000001
-    STATE_SUBS = 0b00000010 
-    STATE_AUTH = 0b00000100 
-    STATE_NOTI = 0b00001000 
-    STATE_DIFF = 0b00010000 
+    STATE_SUBS = 0b00000010
+    STATE_AUTH = 0b00000100
+    STATE_NOTI = 0b00001000
+    STATE_DIFF = 0b00010000
     STATE_OK   = 0b11111111
 
     def __init__(self, loop, pool, user, worker, passwd, rate):
@@ -107,10 +107,10 @@ class StratumClientProtocol(asyncio.Protocol):
 
     def stratum_send_subscribe(self, transport):
         self.subs_stratum_id = self._stratum_send_request(transport, 'mining.subscribe', [])
-    
+
     def stratum_send_authorize(self, transport):
         self.auth_stratum_id = self._stratum_send_request(transport, 'mining.authorize', [self.worker_name, self.passwd])
-    
+
     def stratum_send_submit(self, transport):
         ntime = "%0.8x"%int(time.time())
         if self.session.job is None:
@@ -118,7 +118,7 @@ class StratumClientProtocol(asyncio.Protocol):
         job_id = self.session.job.job_id
         exn2 = ''.join([random.choice('0123456789abcdef') for _ in range(self.session.exn2_size * 2)])
         nounce = ''.join([random.choice('0123456789abcdef') for _ in range(8)])
-        self._stratum_send_request(transport, 'mining.submit', [self.worker_name, job_id, exn2, ntime, nounce])
+        self._stratum_send_request(transport, 'mining.submit', [self.worker_name, job_id, exn2, self.session.job.ntime, nounce])
         self.session.submit_share()
 
     def connection_made(self, transport):
@@ -128,9 +128,9 @@ class StratumClientProtocol(asyncio.Protocol):
         self.stratum_send_authorize(transport)
 
     def _handle_mining_notify(self, msg):
-        self.session.set_job(msg['params']) 
+        self.session.set_job(msg['params'])
         logging.debug("MINING_NOTIFY: %s", self.session.job)
-    
+
     def _handle_mining_set_difficulty(self, msg):
         self.session.set_diff(int(msg['params'][0]))
 
@@ -139,7 +139,7 @@ class StratumClientProtocol(asyncio.Protocol):
             return msg['result']
         except ValueError:
             return False
-    
+
     def _handle_mining_subs_response(self, msg):
         try:
             _, exn1, exn2_size = msg['result']
@@ -167,7 +167,7 @@ class StratumClientProtocol(asyncio.Protocol):
         if msg['id'] == self.auth_stratum_id and self.state & StratumClientProtocol.STATE_SUBS > 0:
             if self._handle_mining_auth_response(msg):
                 self.state |= StratumClientProtocol.STATE_AUTH
-    
+
     def do_in_period(self, transport):
         if not self.state == StratumClientProtocol.STATE_OK:
             self.timeout -= 1
@@ -195,14 +195,17 @@ def send_shares(clients):
     while True:
         for transport, protocol in clients:
             protocol.do_in_period(transport)
-        yield from asyncio.sleep(0.05)
+        yield from asyncio.sleep(0.03)
 
 
 loop = asyncio.get_event_loop()
 
 miners = [
     ['Bixin', 'hyper', 's5', '123', (100, 'T')],
-]
+    ['Bixin', 'hyper', 's5', '123', (4.7, 'T')],
+    ['Bixin', 'hyper', 's7', '123', (500, 'T')],
+    ['Bixin', 'hyper', 's7', '123', (1, 'P')],
+]*1000
 
 clients = []
 for miner in miners:
